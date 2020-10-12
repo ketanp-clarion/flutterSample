10b3e63be413cb18cfdc73a5500fc085f39beaa7
Ketan Clarion <>
2020-10-12 17:55:09 +0530

    test CI

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'FLutter CI test',
+        tooltip: 'FLutter CI',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
